87263e317375cc6959e9606dd7e84aed9258a5b3
painye <1850384745@qq.com>
2024-07-01 22:11:42 +0800

    feature(mybatis-plus测试): 测试mybatis-plus操作数据库

---
+++ src/test/java/com/painye/usercenter/service/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.painye.usercenter.service;
+
+import com.painye.usercenter.model.domain.User;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author pan
+ * @date 2024/7/2 7:44
+ */
+@SpringBootTest
+public class UserServiceTest {
+
+    @Resource
+    private UserService userService;
+
+    @Test
+    public void testAddUser(){
+        User user = new User();
+        user.setUserName("t1");
+        user.setUserAccount("t1");
+        user.setAvatarUrl("");
+        user.setGender(0);
+        user.setUserPassword("t1");
+        user.setPhone("111111111");
+        user.setEmail("1@2.com");
+        user.setUserStatus(0);
+        user.setIsDelete(0);
+        user.setUserRole(0);
+        user.setPlanetCode("");
+        boolean save = userService.save(user);
+        Assertions.assertTrue(save);
+    }
+}
